cd12b5f6dbac58f5ba7b37d3e4257f3e18fdae57
manpreet4117 <51238855+manpreet4117@users.noreply.github.com>
2019-05-31 13:35:40 -0400

    Main test

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //This is the testing for manpreet
+        //This is the testing.
     }
 }
